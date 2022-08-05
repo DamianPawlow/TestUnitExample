e85be4cfcaeb4f002c1ec4bc840c5ec1b257300a
unknown <damian.pawlow@gmail.com>
2022-08-05 18:23:21 +0200

    Defining wrong assertion

--- TestUnitExample/UnitTest1.cs
+++ TestUnitExample/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TestUnitExample
         [Test]
         public void Test1()
         {
-            Assert.IsTrue(Calculator.Addition(2,2) == 4);
+            Assert.IsTrue(Calculator.Addition(2,2) == 3);
             Assert.AreEqual(4, Calculator.Addition(1, 3));
             Assert.AreNotEqual(8, Calculator.Addition(1, 6));
             Assert.IsInstanceOf<int>(Calculator.Addition(2, 3));
