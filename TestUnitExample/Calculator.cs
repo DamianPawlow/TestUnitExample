35810e8cf9f0ecebdc227320e9ddfb1c89f87bc8
Eliza Zych <elizazych@MacBook-Pro-Eliza.local>
2022-08-05 10:01:16 +0200

    Project with test units in Nunit to simple Calculator class

---
+++ TestUnitExample/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestUnitExample
+{
+    public class Calculator
+    {
+        public static int Addition(int numerOne, int numberTwo)
+        {
+            return numerOne + numberTwo;
+        }
+    }
+}
---
+++ TestUnitExample/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TestUnitExample
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(Calculator.Addition(2,2) == 4);
+            Assert.AreEqual(4, Calculator.Addition(1, 3));
+            Assert.AreNotEqual(2, Calculator.Addition(1, 7));
+            Assert.IsInstanceOf<int>(Calculator.Addition(2, 3));
+        }
+    }
+}
